edb51bd10ce918f501d0d9ef04a7c2c990c66525
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-08 12:28:11 +0100

    tipusok

---
+++ JS/Alapok/2. Tipusok/script.js
@@ -0,0 +1,11 @@
+// Típusok
+
+// Tipp:
+// int, string, char, double, boolean, float, list, struct
+
+// Valóság:
+// number => int, float, double
+// string => string, char
+// boolean => boolean
+// array => list
+// object => struct
