aafd8d7233fdc63ebfaf6a56ec0bdab2fb4f4fd3
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-14 12:27:13 +0100

    function, if, else

---
+++ JS/Alapok/4. Vezérlés/script.js
@@ -0,0 +1,63 @@
+// Vezérlési szerkezetek
+// if, for, while, switch => C# szintaxis
+// function
+
+// 1. Függvények
+// Eljárás: undefined visszatérésű függvény
+// paraméterek típusa nem érdekes
+function atlag(a, b) {
+    return (a + b) / 2;
+}
+
+
+// 2. Ciklusok, láthatóság (var, let)
+
+
+
+
+// 3. If, "truthy, falsy"
+// Falsy: hamisra kiértékelődő
+// Nekünk: false, 0, "", null, undefined, NaN
+if (1 < 2) {
+    console.log("Igaz!");
+} else {
+    console.log("Hamis!");
+}
+
+
+
+// ---------------------------------------
+
+// 4. Készíts duplazoFelezo(n) nevű függvényt, ami:
+// páros számok felét,
+// páratlanok dupláját adja vissza!
+
+
+
+
+// 5. Készíts osztokSzama(n) nevű függvény, amely:
+// megadja, hogy hány pozitív osztója van n-nek!
+
+
+
+
+// 6. Készíts primE(p) nevű függvényt, amely
+// egy számról eldönti, hogy prímszám-e!
+// Visszatérési értéke pontosan akkor igaz
+// ha prímszám a paraméter!
+// Csak 1-nél nagyobb egész számok lehetnek prímek!
+
+
+
+
+// 7. Készíts szoveg(a, b) néven függvényt, amely
+// egy [a, b] intervallumból előállít egy neki
+// megfelelő hosszúságú stringet.
+// Ha az intervallum egy eleme osztható 2-vel, akkor "2"
+// ha osztható 3-mal akkor "3",
+// ha 2-vel és 3-mal is osztható akkor "X",
+// minden egyéb esetben pedig "-" legyen a neki megfelelő karakter!
+// Pl.: szoveg(12, 20) === "X-232-X-2"
+
+
+
