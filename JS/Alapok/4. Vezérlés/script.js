a5f3703a567b12f489b05fba04332a145e962e5e
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-19 11:34:50 +0100

    if, for, function

---
+++ JS/Alapok/4. Vezérlés/script.js
@@ -0,0 +1,73 @@
+// Vezérlési szerkezetek
+// if, for, while, switch => C# szintaxis
+// function
+
+// 1. Függvények
+// A "return" sor végén mindig van egy
+// automatikus pontosvessző!
+function atlag(a, b) {
+    let atl = (a + b) / 2;
+    return atl;
+}
+
+
+// 2. Ciklusok, láthatóság (var, let)
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+// console.log(i);
+
+
+// 3. If, "truthy, falsy"
+// Falsy: hamisra kiértékelődő
+// Nekünk: false, 0, "", null, undefined, NaN
+// if (2 < 3) {
+//     console.log("Kisebb!")
+// } else if (2 === 3) {
+//     console.log("Egyenlő!")
+// } else {
+//     console.log("Nagyobb!")
+// }
+if (1 / 0) {
+    console.log("Igaz!");
+} else {
+    console.log("Hamis!");
+}
+console.clear();
+
+
+// ---------------------------------------
+
+// 4. Készíts duplazoFelezo(n) nevű függvényt, ami:
+// páros számok felét,
+// páratlanok dupláját adja vissza!
+
+
+
+
+// 5. Készíts osztokSzama(n) nevű függvény, amely:
+// megadja, hogy hány pozitív osztója van n-nek!
+
+
+
+
+// 6. Készíts primE(p) nevű függvényt, amely
+// egy számról eldönti, hogy prímszám-e!
+// Visszatérési értéke pontosan akkor igaz
+// ha prímszám a paraméter!
+// Csak 1-nél nagyobb egész számok lehetnek prímek!
+
+
+
+
+// 7. Készíts szoveg(a, b) néven függvényt, amely
+// egy [a, b] intervallumból előállít egy neki
+// megfelelő hosszúságú stringet.
+// Ha az intervallum egy eleme osztható 2-vel, akkor "2"
+// ha osztható 3-mal akkor "3",
+// ha 2-vel és 3-mal is osztható akkor "X",
+// minden egyéb esetben pedig "-" legyen a neki megfelelő karakter!
+// Pl.: szoveg(12, 20) === "X-232-X-2"
+
+
+
